f6b4c41ca917a00a7969fe2d13ce65a3e633eb43
Gaurav Chaudhary <gauravchaudhary3000@gmail.com>
2025-01-22 00:39:58 +0530

    added cookie-parser middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
 import router from "./routers";
+import cookieParser from "cookie-parser";
 import { config } from "./config";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", router);
 
